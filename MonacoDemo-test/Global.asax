e9834bc9f57962962281a003b9ac55e9ac7a82fd
Lohith <kashyapa78@hotmail.com>
2013-12-14 10:40:50 +0530

    initial commit
    initial commit

---
+++ MonacoDemo-test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MonacoDemo_test.MvcApplication" Language="C#" %>
---
+++ MonacoDemo-test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MonacoDemo_test.Startup))]
+namespace MonacoDemo_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
